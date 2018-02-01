b63a2508d0827688b578fb80fa6e6863620277a0
Dimitris Dimou <mijuomij@gmail.com>
2018-02-01 14:06:41 +0200

    include

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,66 +4,47 @@
 void computeNormals(const pcl::PolygonMesh &mesh,
 		    pcl::PointCloud<pcl::PointXYZ> &cloud,
 		    pcl::PointCloud<pcl::Normal> &normals) {
-	pcl::fromPCLPointCloud2(mesh->cloud, cloud);
-	pcl::features::computeApproximateNormals(cloud, mesh->polygons,
+	pcl::fromPCLPointCloud2(mesh.cloud, cloud);
+	pcl::features::computeApproximateNormals(cloud, mesh.polygons,
 						 normals);
 }
 
 void centerCloud(pcl::PointCloud<pcl::PointXYZ> &cloud) {
 	pcl::CentroidPoint<pcl::PointXYZ> centroid;
-	for (int i = 0; i < cloud->points.size(); ++i) {
-		centroid.add(cloud->points[i]);
+	for (int i = 0; i < cloud.points.size(); ++i) {
+		centroid.add(cloud.points[i]);
 	}
 	pcl::PointXYZ c;
 	centroid.get(c);
 
-	for (int i = 0; i < cloud->points.size(); ++i) {
-		cloud->points[i].x = cloud->points[i].x - c.x;
-		cloud->points[i].y = cloud->points[i].y - c.y;
-		cloud->points[i].z = cloud->points[i].z - c.z;
+	for (int i = 0; i < cloud.points.size(); ++i) {
+		cloud.points[i].x = cloud.points[i].x - c.x;
+		cloud.points[i].y = cloud.points[i].y - c.y;
+		cloud.points[i].z = cloud.points[i].z - c.z;
 	}
 }
 
 void fitToUnitCloud(pcl::PointCloud<pcl::PointXYZ> &cloud) {
 	float maxDist = 0.0;
 
-	for (int i = 0; i < cloud->points.size(); ++i) {
-		float dist = sqrt(pow(cloud->points[i].x, 2) +
-				  pow(cloud->points[i].y, 2) +
-				  pow(cloud->points[i].z, 2));
+	for (int i = 0; i < cloud.points.size(); ++i) {
+		float dist = sqrt(pow(cloud.points[i].x, 2) +
+				  pow(cloud.points[i].y, 2) +
+				  pow(cloud.points[i].z, 2));
 
 		if (dist > maxDist) maxDist = dist;
 	}
 
-	for (int i = 0; i < cloud->points.size(); ++i) {
-		cloud->points[i].x /= maxDist;
-		cloud->points[i].y /= maxDist;
-		cloud->points[i].z /= maxDist;
+	for (int i = 0; i < cloud.points.size(); ++i) {
+		cloud.points[i].x /= maxDist;
+		cloud.points[i].y /= maxDist;
+		cloud.points[i].z /= maxDist;
 	}
 }
 
 void normalizeCloud(pcl::PointCloud<pcl::PointXYZ> &cloud) {
 	centerCloud(cloud);
 	fitToUnitCloud(cloud);
-}
-
-template <typename Cloud, typename CloudNormals>
-pcl::PointCloud<pcl::FPFHSignature33> execute_featureEstimation(
-    Cloud &cloud, CloudNormals &normals) {
-	pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::FPFHSignature33>
-	    fpfh;
-	fpfh.setInputCloud(cloud);
-	fpfh.setInputNormals(normals);
-	pcl::search::KdTree<pcl::PointXYZ> tree(
-	    pcl::search::KdTree<pcl::PointXYZ>);
-	fpfh.setSearchMethod(tree);
-	pcl::PointCloud<pcl::FPFHSignature33> fpfhs(
-	    pcl::PointCloud<pcl::FPFHSignature33>());
-
-	fpfh.setRadiusSearch(0.05);
-	fpfh.compute(*fpfhs);
-
-	return fpfhs;
 }
 
 void enterViewerLoop(pcl::PointCloud<pcl::PointXYZ> cloud,
