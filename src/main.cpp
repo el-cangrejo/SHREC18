88a15d02c8bcc28c7ecd4894162e318c38394a5f
Iason Manolas <iasonmanolasece@gmail.com>
2018-02-01 15:25:19 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,21 @@
 int main(int argc, char **argv) {
 	std::cout << "Hello SHREC2018!\n";
 
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  pcl::PointCloud<pcl::Normal> normals;
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	pcl::PointCloud<pcl::Normal> normals;
 	pcl::PolygonMesh mesh;
 
-  std::string queryModel = "shrec18_recognition/Queries/2.ply";
-  std::string dataModel = "shrec18_recognition/Dataset/1.ply";
+	std::string queryModel = "shrec18_recognition/Queries/2.ply";
+	// std::string dataModel = "shrec18_recognition/Dataset/1.ply";
 
-  if (pcl::io::loadPLYFile(dataModel, mesh) == -1) {
-    return -1;
-  }
+	if (pcl::io::loadPLYFile(queryModel, mesh) == -1) {
+		return -1;
+	}
 
-  std::cout << "Loaded query mesh with: " << mesh.cloud.data.size() << " data points" << std::endl;
+	std::cout << "Loaded query mesh with: " << mesh.cloud.data.size()
+		  << " data points" << std::endl;
 
 	normalizeCloud(cloud);
 	computeNormals(mesh, cloud, normals);
-  enterViewerLoop(cloud, normals);
+	enterViewerLoop(cloud, normals);
 }
