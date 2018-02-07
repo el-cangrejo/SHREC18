f70d43ff5e16b1fa554cfb38126e48b3c3d0deff
Iason Manolas <iasonmanolasece@gmail.com>
2018-02-07 10:16:42 +0200

    Refactored

--- src/comparison.h
+++ src/comparison.h
@@ -3,7 +3,7 @@
 #include <pcl/features/from_meshes.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/features/pfh.h>
-#include <pcl/features/shot.h>
+#include <pcl/features/shot_omp.h>
 #include <pcl/point_types.h>
 
 struct CloudWithNormals {
@@ -23,7 +23,7 @@ pcl::PointCloud<pcl::PFHSignature125> computeFeatures_PFH(
 	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(
 	    new pcl::search::KdTree<pcl::PointXYZ>());
 	pfh.setSearchMethod(tree);
-	//pfh.setRadiusSearch(searchRadius);
+	// pfh.setRadiusSearch(searchRadius);
 	pfh.setKSearch(searchRadius);
 	pcl::PointCloud<pcl::PFHSignature125> outputFeatures;
 	pfh.compute(outputFeatures);
@@ -47,32 +47,32 @@ pcl::PointCloud<pcl::FPFHSignature33> computeFeatures_FPFH(
 
 pcl::PointCloud<pcl::SHOT352> computeFeatures_SHOT352(
     const CloudWithNormals input, float searchRadius) {
-	pcl::SHOTEstimation<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> fpfh;
+	pcl::SHOTEstimationOMP<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> fpfh;
 	fpfh.setInputCloud(input.cloudPositions.makeShared());
 	fpfh.setInputNormals(input.cloudNormals.makeShared());
 	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(
 	    new pcl::search::KdTree<pcl::PointXYZ>());
 	fpfh.setSearchMethod(tree);
-	fpfh.setRadiusSearch(0.3);
+	fpfh.setKSearch(0);
+	fpfh.setRadiusSearch(searchRadius);
 	pcl::PointCloud<pcl::SHOT352> outputFeatures;
 	fpfh.compute(outputFeatures);
 	return outputFeatures;
 }
 
 template <typename FeatureType, typename FeatureSize>
-float l2FeatureDistance(FeatureType first, FeatureType second,
-			FeatureSize featureSize) {
+float l2FeatureDistance(FeatureType first, FeatureType second) {
 	float distance = 0.0;
+	int featureSize = FeatureType::featureSize();
 
 	for (int i = 0; i < featureSize; ++i)
 		distance += pow(first.histogram[i] - second.histogram[i], 2);
 	return sqrt(distance);
 }
 
-float l2FeatureDistance(pcl::SHOT352 first, pcl::SHOT352 second,
-			int featureSize) {
+float l2FeatureDistance(pcl::SHOT352 first, pcl::SHOT352 second) {
 	float distance = 0.0;
-
+	int featureSize = 352;
 	for (int i = 0; i < featureSize; ++i)
 		distance += pow(first.descriptor[i] - second.descriptor[i], 2);
 	return sqrt(distance);
@@ -91,24 +91,25 @@ std::vector<float> selfFeatureDistance(
 	return distances;
 }
 
-template <typename FeatureType, typename FeatureSize>
+template <typename FeatureType>
 std::vector<float> computeFeatureDistancesFromTargetModel(
-    pcl::PointCloud<FeatureType> targetFeatures, FeatureType queryFeature,
-    FeatureSize featureSize) {
-	std::vector<float> outputDistances;
+    pcl::PointCloud<FeatureType> targetFeatures, FeatureType queryFeature) {
+	std::vector<float> outputDistances(targetFeatures.points.size());
 	for (int i = 0; i < targetFeatures.points.size(); ++i) {
-		float dist = l2FeatureDistance(
-		    queryFeature, targetFeatures.points[i], featureSize);
+		float dist =
+		    l2FeatureDistance(queryFeature, targetFeatures.points[i]);
 		outputDistances.push_back(dist);
 	}
 	return outputDistances;
 }
 
-std::vector<float> histogramDifference(float *first, float *second, int histogramSize) {
+std::vector<float> histogramDifference(float *first, float *second,
+				       int histogramSize) {
 	std::vector<float> output(histogramSize);
 
-	for(int binIndex = 0; binIndex < histogramSize; binIndex++) {
-		float binDifference=std::abs(first[binIndex] - second[binIndex]);
+	for (int binIndex = 0; binIndex < histogramSize; binIndex++) {
+		float binDifference =
+		    std::abs(first[binIndex] - second[binIndex]);
 		output[binIndex] = binDifference;
 	}
 	return output;
@@ -116,19 +117,40 @@ std::vector<float> histogramDifference(float *first, float *second, int histogra
 
 pcl::PointCloud<pcl::PFHSignature125> computePointFeatures_PFH(
     const CloudWithNormals input, int idx, float searchRadius) {
-	pcl::PFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::PFHSignature125> pfh;
+	pcl::PFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::PFHSignature125>
+	    pfh;
 	pfh.setInputCloud(input.cloudPositions.makeShared());
 	pfh.setInputNormals(input.cloudNormals.makeShared());
-	boost::shared_ptr<std::vector<int> > indicesptr (new std::vector<int> {idx});
+	boost::shared_ptr<std::vector<int> > indicesptr(
+	    new std::vector<int>{idx});
 	pfh.setIndices(indicesptr);
 
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(
+	    new pcl::search::KdTree<pcl::PointXYZ>());
 	pfh.setSearchMethod(tree);
-	//pfh.setRadiusSearch(searchRadius);
+	// pfh.setRadiusSearch(searchRadius);
 	pfh.setKSearch(searchRadius);
 	pcl::PointCloud<pcl::PFHSignature125> outputFeatures;
 	pfh.compute(outputFeatures);
-	std::cout << "PFH size : " << outputFeatures.points.size() << "\n";
 	return outputFeatures;
 }
 
+pcl::PointCloud<pcl::SHOT352> computePointFeatures_SHOT(
+    const CloudWithNormals input, int idx, float searchRadius) {
+	pcl::SHOTEstimationOMP<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> pfh;
+	pfh.setInputCloud(input.cloudPositions.makeShared());
+	pfh.setInputNormals(input.cloudNormals.makeShared());
+	boost::shared_ptr<std::vector<int> > indicesptr(
+	    new std::vector<int>{idx});
+	pfh.setIndices(indicesptr);
+
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(
+	    new pcl::search::KdTree<pcl::PointXYZ>());
+	pfh.setSearchMethod(tree);
+	// pfh.setRadiusSearch(searchRadius);
+	pfh.setKSearch(0);
+	pfh.setRadiusSearch(searchRadius);
+	pcl::PointCloud<pcl::SHOT352> outputFeatures;
+	pfh.compute(outputFeatures);
+	return outputFeatures;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,55 @@
 #include <iostream>
-#include <utils.hpp>
 #include "comparison.h"
+#include "utils.h"
 
-int main(int argc, char **argv) {
-	std::cout << "Hello SHREC2018!\n";
-
-	std::string queryModel = "shrec18_recognition/Dataset/3.ply";
-	//std::string queryModel = "shrec18_recognition/Queries/2.ply";
+int main(int argc, char** argv) {
+	std::string queryFilepath = "shrec18_recognition/Queries/3.ply";
 	pcl::PointCloud<pcl::PointXYZ> queryCloud;
 	pcl::PointCloud<pcl::Normal> queryNormals;
 	pcl::PolygonMesh queryMesh;
-
-	//std::string targetModel = "shrec18_recognition/Dataset/6.ply";
-	//pcl::PointCloud<pcl::PointXYZ> targetCloud;
-	//pcl::PointCloud<pcl::Normal> targetNormals;
-	//pcl::PolygonMesh targetMesh;
-
-	//// comparison parameters
-	//float searchQueryRadius = 1.5;
-	//float searchTargetRadius = 0.5;
-	//int targetPointIndex = 2000;
-
-	if (pcl::io::loadPLYFile(queryModel, queryMesh) == -1) {
+	if (pcl::io::loadPLYFile(queryFilepath, queryMesh) == -1) {
 		return -1;
-	}
-	//if (pcl::io::loadPLYFile(targetModel, targetMesh) == -1) {
-	//	return -1;
-	//}
-
-	std::cout << "Query model : " << queryMesh.cloud.data.size() << "\n";
-	//std::cout << "Target model : " << targetMesh.cloud.data.size() << "\n";
-	//
+	} else
+		std::cout << "Query model : " << queryMesh.cloud.data.size()
+			  << "\n";
 	pcl::fromPCLPointCloud2(queryMesh.cloud, queryCloud);
 	computeNormals(queryMesh, queryCloud, queryNormals);
 	CloudWithNormals queryCloudWithNormals(queryCloud, queryNormals);
-	//auto queryFeatures =
+
+	float radius = 0.10;
+	int index = 10000;
+	std::cout << "Computing features.." << std::endl;
+	auto queryFeatures =
+	    computeFeatures_SHOT352(queryCloudWithNormals, radius);
+
+	std::cout << "Computing distances.." << std::endl;
+	std::vector<float> distances =
+	    selfFeatureDistance(queryFeatures, index);
+
+	pcl::PointCloud<pcl::PointXYZRGB> rgbCloud;
+	std::cout << "Creating RGB cloud.." << std::endl;
+	createRGBCloud(queryCloud, distances, rgbCloud);
+
+	auto& cloudToVisualize = rgbCloud;
+	normalizeCloud(cloudToVisualize);
+	// enterViewerLoop(queryMesh);
+	enterViewerLoop(cloudToVisualize, rgbCloud[index], radius);
+
+	// std::string targetModel = "shrec18_recognition/Dataset/1.ply";
+	// pcl::PointCloud<pcl::PointXYZ> targetCloud;
+	// pcl::PointCloud<pcl::Normal> targetNormals;
+	// pcl::PolygonMesh targetMesh;
+	// if (pcl::io::loadPLYFile(targetModel, targetMesh) == -1) {
+	// return -1;
+	// }
+	// std::cout << "Target model : " << targetMesh.cloud.data.size() <<
+	// "\n";
+	//
+	// auto queryFeatures =
+	//    k
 	//    computeFeatures_SHOT352(queryCloudWithNormals, searchQueryRadius);
 
-	//pcl::fromPCLPointCloud2(targetMesh.cloud, targetCloud);
-	//computeNormals(targetMesh, targetCloud, targetNormals);
-	//CloudWithNormals targetCloudWithNormals(targetCloud, targetNormals);
-	//auto targetFeatures =
-	//    computeFeatures_SHOT352(targetCloudWithNormals, searchTargetRadius);
-
-	//int featureSize = 352;
-	//std::vector<float> distances = computeFeatureDistancesFromTargetModel(
-	//    targetFeatures, queryFeatures[targetPointIndex], featureSize);
-	//pcl::PointCloud<pcl::PointXYZRGB> rgbCloud;
-	//createRGBCloud(targetCloud, distances, rgbCloud);
-
-	int idx = 0;
-	float inner_radius = 0.5;
-	float outer_radius = 1.5;
-	auto pfh_inner = computePointFeatures_PFH(queryCloudWithNormals, idx, 20);
-	auto pfh_outer = computePointFeatures_PFH(queryCloudWithNormals, idx, 40);
-
-	auto h = histogramDifference(pfh_inner.points[idx].histogram, pfh_outer.points[idx].histogram, 125);
-
-	for (auto i : h)
-		std::cout << i << "\n";
-
-	enterViewerLoop(queryCloud, queryNormals, queryCloud.points[idx], inner_radius);
+	// pcl::fromPCLPointCloud2(queryMesh.cloud, targetCloud);
+	// computeNormals(targetMesh, targetCloud, targetNormals);
+	// CloudWithNormals targetCloudWithNormals(targetCloud, targetNormals);
 }
